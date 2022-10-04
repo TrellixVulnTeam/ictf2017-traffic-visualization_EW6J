bedd019a2698d5b33691c242c5e3bc19267e71af
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 20:49:13 +0000

    Adding numeric_owner as keyword arguement

--- src/preprocessing/pixz.py
+++ src/preprocessing/pixz.py
@@ -52,7 +52,7 @@ def extract_pcap(archive, filename, out_directory):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, path=out_directory)
